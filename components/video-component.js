b4dea47fdff1528062c96c5684f40cfca3971343
claudia15-rd <146999902+claudia15-rd@users.noreply.github.com>
2024-04-27 18:34:01 +0200

    Add files via upload

---
+++ components/video-component.js
@@ -0,0 +1,12 @@
+import React, {Component, useState, useEffect} from 'react'
+import { Text,StyleSheet,View } from 'react-native'
+
+export default function videoComponent() {
+    return (
+        <View>
+            <Text> Pruebas Video </Text>
+        </View>
+    )
+}
+
+const styless = StyleSheet.create({})
